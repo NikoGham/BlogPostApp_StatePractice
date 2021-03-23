6976e0cba76dabd2219381bdd877c4b70bab44fd
Niko Gham <ng@nickgillham.co.uk>
2021-03-23 18:56:32 +0000

    init

---
+++ App.js
@@ -0,0 +1,29 @@
+import { createAppContainer } from 'react-navigation'
+import { createStackNavigator } from 'react-navigation-stack'
+import React from 'react'
+import IndexScreen from './src/screens/indexScreen'
+import { Provider } from './src/context/BlogContext'
+import ShowScreen from './src/screens/ShowScreen'
+
+const navigator = createStackNavigator(
+ {
+  Index: IndexScreen,
+  Show: ShowScreen,
+ },
+ {
+  initialRouteName: 'Index',
+  defaultNavigationOptions: {
+   title: 'Blogs',
+  },
+ }
+)
+
+const App = createAppContainer(navigator)
+
+export default () => {
+ return (
+  <Provider>
+   <App />
+  </Provider>
+ )
+}
---
+++ src/context/BlogContext.js
@@ -0,0 +1,33 @@
+import createDataContext from './createDataContext'
+
+const blogReducer = (state, action) => {
+ switch (action.type) {
+  case 'DELETE_BLOGPOST':
+   return state.filter((blogPost) => blogPost.id !== action.payload)
+  case 'ADD_BLOGPOST':
+   return [
+    ...state,
+    { id: Math.floor(Math.random() * 99999), title: `Blog Post #${state.length + 1}` },
+   ]
+  default:
+   return state
+ }
+}
+
+const addBlogPost = (dispatch) => {
+ return () => {
+  dispatch({ type: 'ADD_BLOGPOST' })
+ }
+}
+
+const deleteBlogPost = (dispatch) => {
+ return (id) => {
+  dispatch({ type: 'DELETE_BLOGPOST', payload: id })
+ }
+}
+
+export const { Context, Provider } = createDataContext(
+ blogReducer,
+ { addBlogPost, deleteBlogPost },
+ []
+)
